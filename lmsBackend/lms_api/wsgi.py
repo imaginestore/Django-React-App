6e267df99a269e3d3a3053b993a9f0d37a239493
Irshad Shaikh <ishaikh16@yahoo.com>
2024-10-10 15:05:39 +0200

    sys.path removed from wsgi.py file

--- lmsBackend/lms_api/wsgi.py
+++ lmsBackend/lms_api/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_api.settings')
