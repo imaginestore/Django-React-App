37e02c02162d7f0de643f597c5ec8ef6025ff32c
Irshad Shaikh <ishaikh16@yahoo.com>
2024-10-10 20:13:20 +0200

    path DJANGO_SETTINGS_MODULE modified to absolute path in manage.py

--- lmsBackend/manage.py
+++ lmsBackend/manage.py
@@ -3,11 +3,9 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lmsBackend.lms_api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
